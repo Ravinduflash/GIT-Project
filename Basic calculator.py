316e9ab3b772b6e09e8882ab801c9cdd8b994c4e
malithi19 <malithiranawaka8@gmail.com>
2024-05-21 19:34:34 +0530

    Update Basic calculator.py

--- Basic calculator.py
+++ Basic calculator.py
@@ -5,4 +5,13 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
     return a / b
+
+print(add(3, 2))        
+print(subtract(3, 2))   
+print(multiply(3, 2))   
+print(divide(3, 2))    
+print(divide(3, 0))  
+
